f6a79bbdc68c7b249f53d187a5f74e138df00ff5
Connor Manning <connor@hobu.co>
2019-03-28 11:02:16 -0500

    Update potree bundle - closes #2.

--- build/potree/potree.js
+++ build/potree/potree.js
@@ -3586,7 +3586,7 @@
 			this.type = 'ept';
 
 			this.schema = schema;
-			this.span = info.span;
+			this.span = info.span || info.ticks;
 			this.boundingBox = U.toBox3(bounds);
 			this.tightBoundingBox = U.toBox3(boundsConforming);
 			this.offset = U.toVector3([0, 0, 0]);
@@ -11391,8 +11391,12 @@ void main() {
 					exports.lru.touch(node);
 					this.highestLevelServed = Math.max(node.getLevel(), this.highestLevelServed);
 
-					let doTraverse = (node.level % node.pcoGeometry.hierarchyStepSize) === 0 && node.hasChildren;
-					doTraverse = doTraverse || node.getLevel() === 0;
+					var geom = node.pcoGeometry;
+					var hierarchyStepSize = geom ? geom.hierarchyStepSize : 1;
+
+					var doTraverse = node.getLevel() === 0 ||
+						(node.level % hierarchyStepSize === 0 && node.hasChildren);
+
 					if (doTraverse) {
 						this.traverse(node);
 					}
@@ -11456,7 +11460,7 @@ void main() {
 					view[i * 3 + 0],
 					view[i * 3 + 1],
 					view[i * 3 + 2]);
-			
+
 				pos.applyMatrix4(matrix);
 				let distance = Math.abs(segment.cutPlane.distanceToPoint(pos));
 				let centerDistance = Math.abs(segment.halfPlane.distanceToPoint(pos));
@@ -11520,7 +11524,7 @@ void main() {
 
 						let start = new THREE.Vector3(segment.start.x, segment.start.y, bsWorld.center.z);
 						let end = new THREE.Vector3(segment.end.x, segment.end.y, bsWorld.center.z);
-							
+
 						let closest = new THREE.Line3(start, end).closestPointToPoint(bsWorld.center, true, new THREE.Vector3());
 						let distance = closest.distanceTo(bsWorld.center);
 
@@ -11594,7 +11598,7 @@ void main() {
 						for(let i = 0; i < accepted.length; i++){
 
 							let index = accepted[i];
-							
+
 							let start = index * numElements;
 							let end = start + numElements;
 							let sub = source.subarray(start, end);
@@ -17981,8 +17985,7 @@ void main() {
 			if (!this.sceneProjection) {
 				try {
 					this.setSceneProjection(pointcloud.projection);
-				}
-				catch (e) {
+				}catch (e) {
 					console.log('Failed projection:', e);
 
 					if (pointcloud.fallbackProjection) {
@@ -17990,12 +17993,13 @@ void main() {
 							console.log('Trying fallback projection...');
 							this.setSceneProjection(pointcloud.fallbackProjection);
 							console.log('Set projection from fallback');
-						}
-						catch (e) {
+						}catch (e) {
 							console.log('Failed fallback projection:', e);
+							return;
 						}
+					}else{
+						return;
 					}
-					else return;
 				}
 			}
 
@@ -21179,7 +21183,7 @@ ENDSEC
 					let range = material.elevationRange;
 
 					panel.find('#lblHeightRange').html(`${range[0].toFixed(2)} to ${range[1].toFixed(2)}`);
-					panel.find('#sldHeightRang').slider({min: bMin, max: bMax, values: range});
+					panel.find('#sldHeightRange').slider({min: bMin, max: bMax, values: range});
 				};
 
 				let updateIntensityRange = function () {
