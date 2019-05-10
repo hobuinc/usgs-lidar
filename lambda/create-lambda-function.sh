1bb972aa813fd95496b30a3f2c7477737c84f30d
Howard Butler <howard@hobu.co>
2019-05-10 08:15:32 -0500

    use latest PDAL lambda layer

--- lambda/create-lambda-function.sh
+++ lambda/create-lambda-function.sh
@@ -5,7 +5,7 @@
 LAMBDA_FUNCTION_NAME="$1"
 LAMBDA_METHOD_NAME="$2"
 
-PDAL_LAMBDA_LAYER_ARN=" arn:aws:lambda:us-east-1:163178234892:layer:pdal:7"
+PDAL_LAMBDA_LAYER_ARN=" arn:aws:lambda:us-east-1:163178234892:layer:pdal:8"
 LAMDA_EXECUTION_ROLE="lambda_execution"
 
 AWS_PROFILE="hobu"
@@ -31,7 +31,7 @@ aws lambda create-function \
     --profile $AWS_PROFILE \
     --region $AWS_REGION \
     --function-name $LAMBDA_FUNCTION_NAME \
-    --runtime python3.6 \
+    --runtime python3.7 \
     --role $ROLEARN \
     --timeout 200 \
     --memory-size 400 \
