8450344b091ed7457c47ce2c574693f756d1e97f
Howard Butler <howard@hobu.co>
2020-04-10 16:09:27 -0500

    update boundaries

--- boundaries/util/process.py
+++ boundaries/util/process.py
@@ -3,7 +3,7 @@ import osr
 import glob
 import json
 import shutil
-
+ogr.UseExceptions()
 
 web_mercator = osr.SpatialReference()
 web_mercator.ImportFromEPSG(3857)
@@ -12,6 +12,7 @@ web_mercator.ImportFromEPSG(3857)
 geographic = osr.SpatialReference()
 geographic.ImportFromEPSG(4326)
 
+geographic.SetAxisMappingStrategy(osr.OAMS_TRADITIONAL_GIS_ORDER);
 
 jsonfiles = []
 for f in glob.glob("source/*.json"):
@@ -26,15 +27,41 @@ except FileExistsError:
 
 
 
+drv = ogr.GetDriverByName('GeoJSON')
+ds = drv.CreateDataSource('resources.geojson')
+lyr = ds.CreateLayer("resources", geom_type=ogr.wkbMultiPolygon, options=["RFC7946=YES"])
+
+urlField = ogr.FieldDefn("url", ogr.OFTString)
+lyr.CreateField(urlField)
+idField = ogr.FieldDefn("id", ogr.OFTInteger)
+lyr.CreateField(idField)
+
+featureDefn = lyr.GetLayerDefn()
+
+base_url = 'https://s3-us-west-2.amazonaws.com/usgs-lidar-public'
+i = 0
 def process(f):
+    global i
     with open(f) as j:
         data = j.read()
+
+    basename = os.path.basename(f)
+    url = base_url +'/'+ basename.split('.')[0] + '/ept.json'
     j = json.loads(data)
     wkt = j['stages']['filters.hexbin']['boundary']
     g = ogr.CreateGeometryFromWkt(wkt)
     g.AssignSpatialReference(web_mercator)
     g.TransformTo(geographic)
     geojson = g.ExportToJson()
+
+    feature = ogr.Feature(featureDefn)
+    feature.SetGeometry(g)
+    feature.SetField("url", url)
+    feature.SetField("id", i)
+    lyr.CreateFeature(feature)
+
+    feature = None
+    i += 1
 
     output = os.path.join('geojson', os.path.basename(f))
     with open(output,'wb') as j:
